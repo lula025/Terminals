ce533d657f19ed53cc43b3edd0bf79eb274e2967
lula025 <71556675+lula025@users.noreply.github.com>
2020-09-22 21:45:39 +1000

    Update main.c

--- main.c
+++ main.c
@@ -2,64 +2,80 @@
 #include <math.h>
 #define _USE_MATH_DEFINES
 
-typedef struct coplex_number{
- double real,imaginary,module,argument, power,radical;
- }cplx;
- 
+typedef   struct coplex_number{
+        double real,imaginary,module,argument, power,radical;
+    }cplx;
+    
 double trg(cplx z){
  if(z.module==0){
- printf("trigonometric form: NaN\n");
- return 0; 
+    printf("trigonometric form: NaN\n");
+    return 0; 
  } 
  
  z.argument=acos(z.real/z.module);
- 
- if(z.imaginary<0){
- z.argument*=-1;
- }
- printf("trigonometric form: %lf * (cos(%lf)+i*sin(%lf))\n",z.module,z.argument,z.argument);
- return z.argument;
+  
+  if(z.imaginary<0){
+      z.argument*=-1;
+    }
+    printf("trigonometric form: %lf * (cos(%lf)+i*sin(%lf))\n",z.module,z.argument,z.argument);
+    return z.argument;
 }
 int rvrs(cplx z){
- if (z.real==0){
- if(z.imaginary>0){
- printf("reverse: %lf*i\n",-z.imaginary);
- }
- else if(z.imaginary<0){
- printf("reverse: %lf*i\n",-z.imaginary);
- }
- else printf("reverse: %d*i\n",0) ; 
- }
- if(z.imaginary>0){
- printf("reverse: %lf%lf*i\n",z.real,-z.imaginary);
- }
- else if(z.imaginary<0){
- printf("reverse: %lf+%lf*i\n",z.real,-z.imaginary);
- }
- else printf("reverse: %lf\n",z.real);
+    if (z.real==0){
+         if(z.imaginary>0){
+            printf("reverse: %lf*i\n",-z.imaginary);
+         }
+         else if(z.imaginary<0){
+            printf("reverse: %lf*i\n",-z.imaginary);
+         }
+         else printf("reverse: %d*i\n",0) ;  
+    }
+    else if(z.imaginary>0){
+        printf("reverse: %lf%lf*i\n",z.real,-z.imaginary);
+    }
+    else if(z.imaginary<0){
+        printf("reverse: %lf+%lf*i\n",z.real,-z.imaginary);
+    }
+    else printf("reverse: %lf\n",z.real);
 
  }
 int dgr(cplx z){
- if(z.module==0){
- printf("degree: %d\n",0);
- return 0;
- }
- 
- z.argument=z.power*z.argument;
- while(z.argument>M_PI){
- z.argument-=M_PI;
- }
- printf("degree: %lf * (cos(%lf)+i*sin(%lf))\n", pow(z.module,z.power),z.argument,z.argument);
- 
+    if(z.module==0){
+        printf("degree: %d\n",0);
+        return 0;
+    }
+    
+    z.argument=z.power*z.argument;
+    
+    
+    if (z.argument >= M_PI) {
+        while (z.argument >= M_PI)
+            z.argument -= 2 * M_PI;
+    }
+    else if (z.argument < -M_PI) {
+        while(z.argument < -M_PI ){
+            z.argument += 2 * M_PI;
+        }
+    }
+    
+    printf("degree: %lf * (cos(%lf)+i*sin(%lf))\n", pow(z.module,z.power),z.argument,z.argument);
+     
 }
 int rdcl(cplx z){
- 
- z.argument=z.argument/z.radical;
- 
- while(z.argument<2*M_PI){
- printf("radical: %lf * (cos(%lf)+i*sin(%lf))\n",pow(z.module,1/z.radical),z.argument,z.argument);
- z.argument+=2*M_PI/z.radical;
- }
+   
+   for (int k = 0; k < z.radical; k++) {
+       double angle = (z.argument + 2 * M_PI * k) / z.radical;
+       if (angle >= M_PI) {
+            while (angle >= M_PI)
+                angle -= 2 * M_PI;
+        }
+        else if (angle < -M_PI) {
+            while(angle < -M_PI ){
+                angle += 2 * M_PI;
+            }
+        }
+        printf("radical: %lf * (cos(%lf)+i*sin(%lf))\n",pow(z.module,1/z.radical),angle,angle);
+    }
 }
 
 int main() {
@@ -69,11 +85,13 @@ int main() {
 scanf("%lf%lf%lf%lf",&z.real,&z.imaginary,&z.power,&z.radical);
 z.module = sqrt (pow(z.real,2.0)+pow(z.imaginary,2.0));
 
+
 z.argument = trg(z);
 rvrs(z);
 dgr(z);
 rdcl(z);
 
- 
- return 0;
+  
+    return 0;
 }
+
